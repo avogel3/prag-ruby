9db90503783df550228688194157b019eae03c47
Andrew Vogel <andrew.vogel@elvamarketing.com>
2016-10-26 23:32:11 -0400

    Initial Commit

---
+++ studio_game/studio_game.rb
@@ -0,0 +1,11 @@
+first_player = "larry"
+first_player_health = 60
+puts "#{first_player}'s health is #{first_player_health * 3}"
+puts "#{first_player}'s health is #{first_player_health.to_f / 9.to_f}"
+puts "#{first_player}'s health is #{first_player_health / 9.0}"
+
+puts "Players:\n\tLarry\n\tCurly\n\tMoe"
+
+second_player = "curly"
+third_player = "moe"
+puts "Players: \n\t#{first_player} \n\t#{second_player} \n\t#{third_player}"
